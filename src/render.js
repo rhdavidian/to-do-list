ab0c99082cc417a532785ac62d4148a25136318a
Richard Davidian <127055926+rhdavidian@users.noreply.github.com>
2024-05-23 17:23:46 -0500

    Move list of project titles to Nav Bar. Change classes to make more sense. Alter render function to properly work.

--- src/render.js
+++ src/render.js
@@ -1,15 +1,17 @@
 import { chores } from './addDelete'
-const mainList = document.querySelector('.projects');
-const topRow = document.getElementById('projectsTopRow');
+const topRow = document.getElementById('detailsTopRow');
+const navList = document.getElementById('navList')
+// const currentProjects = document.getElementById()
 
 function render(){
-    while (mainList.firstChild) {
-        mainList.removeChild(mainList.firstChild)
+    while (navList.firstChild) {
+        navList.removeChild(navList.firstChild)
     }
-    mainList.appendChild(topRow);
+    // navList.appendChild(navList);
+    navList.textContent = 'Current Projects:';
 
     for (let i = 0; i < chores.length; i++){
-        const project = document.createElement('div');
+        const project = document.createElement('li');
         project.classList.add('project');
         chores.forEach((obj) => {
             if (i === obj.id){
@@ -17,7 +19,7 @@ function render(){
             return project;
             };
         })
-        mainList.appendChild(project);
+        navList.appendChild(project);
 
     //Code below works, modifying to use id property in object.
     // for (let i = 0; i < chores.length; i++){
